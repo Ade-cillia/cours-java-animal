04e6bb5166db283d61399f063f985f0fab46f8a9
Aurélien <aureliendu3809@outlook.fr>
2022-12-11 23:18:46 +0100

    eclipse archict

--- Main.java
+++
@@ -1,9 +0,0 @@
-
-import test.*;
-
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Start app"); 
-    TestIHM.lunchTest();
-  }
-}
---
+++ appClientModule/test/Main.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("All test"); 
+    TestIHM.lunchTest();
+	}
+
+	/* (non-Java-doc)
+	 * @see java.lang.Object#Object()
+	 */
+	public Main() {
+		super();
+	}
+
+}
